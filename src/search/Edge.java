b6bd2969abc1dc11d12f7eb9f3a1b9b117b10fc9
Dufresne <dufresa@essendant.com>
2019-06-07 11:04:51 -0600

    stubbed out edge class for search program

---
+++ src/search/Edge.java
@@ -0,0 +1,82 @@
+package search;
+
+/**
+ * @author dufresa
+ *
+ */
+public class Edge {
+	
+	// FIELDS
+	/**
+	 * ID for edge object
+	 */
+	private final String ID;
+	/**
+	 * Source Vertex of edge object
+	 */
+	private final Vertices SOURCE;
+	/**
+	 * Destination Vertex of edge object
+	 */
+	private final Vertices DESTINATION;
+	/**
+	 * Weight or cost of edge object
+	 */
+	private final int WEIGHT;
+	
+	//CONSTRUCTORS
+	/**
+	 * Constructor for edge object
+	 * 
+	 * @param id
+	 * @param source
+	 * @param destination
+	 * @param weight
+	 */
+	public Edge(String id, Vertices source, Vertices destination, int weight) {
+		this.ID = id;
+		this.SOURCE = source;
+		this.DESTINATION = destination;
+		this.WEIGHT = weight;
+	}
+	
+	// GETTERS
+	/**
+	 * @return String object representing the id of this edge
+	 */
+	public String getID() {
+		return ID;
+	}
+
+	/**
+	 * @return Vertices object of the source of this edge
+	 */
+	public Vertices getSOURCE() {
+		return SOURCE;
+	}
+
+	/**
+	 * @return Vertices object of the destination for this edge
+	 */
+	public Vertices getDESTINATION() {
+		return DESTINATION;
+	}
+
+	/**
+	 * @return int of the weight or cost of this edge
+	 */
+	public int getWEIGHT() {
+		return WEIGHT;
+	}
+
+	// TOSTRING
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Edge [SOURCE=" + SOURCE + ", DESTINATION=" + DESTINATION + "]";
+	}
+	
+	
+}
